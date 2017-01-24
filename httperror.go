e9872d9b4175ae3834223fa2f6609e9a8561f439
acoshift <acoshift@gmail.com>
2017-01-24 17:20:16 +0700

    Add grpc map function

--- httperror.go
+++ httperror.go
@@ -5,6 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 // Error is the httperror's Error
@@ -109,4 +112,51 @@ func InternalServerErrorWith(err error) error {
 // ConflictWith merges error with conflict
 func ConflictWith(err error) error {
 	return Merge(Conflict, err)
+}
+
+// GRPC maps grpc error to http error
+func GRPC(err error) error {
+	if err == nil {
+		return nil
+	}
+	// check is err grpc error
+	desc := grpc.ErrorDesc(err)
+	switch grpc.Code(err) {
+	case codes.OK:
+		return nil
+	case codes.Canceled:
+		return &Error{http.StatusRequestTimeout, "canceled", desc}
+	case codes.Unknown:
+		return &Error{http.StatusInternalServerError, "unknown", desc}
+	case codes.InvalidArgument:
+		return &Error{http.StatusBadRequest, "invalid_argument", desc}
+	case codes.DeadlineExceeded:
+		return &Error{http.StatusRequestTimeout, "deadline_exceeded", desc}
+	case codes.NotFound:
+		return &Error{http.StatusNotFound, "not_found", desc}
+	case codes.AlreadyExists:
+		return &Error{http.StatusConflict, "already_exists", desc}
+	case codes.PermissionDenied:
+		return &Error{http.StatusForbidden, "permission_denied", desc}
+	case codes.Unauthenticated:
+		return &Error{http.StatusUnauthorized, "unauthenticated", desc}
+	case codes.ResourceExhausted:
+		return &Error{http.StatusForbidden, "resource_exhausted", desc}
+	case codes.FailedPrecondition:
+		return &Error{http.StatusPreconditionFailed, "failed_precondition", desc}
+	case codes.Aborted:
+		return &Error{http.StatusConflict, "aborted", desc}
+	case codes.OutOfRange:
+		return &Error{http.StatusBadRequest, "out_of_range", desc}
+	case codes.Unimplemented:
+		return &Error{http.StatusNotImplemented, "unimplemented", desc}
+	case codes.Internal:
+		return &Error{http.StatusInternalServerError, "internal", desc}
+	case codes.Unavailable:
+		return &Error{http.StatusServiceUnavailable, "service_unavailable", desc}
+	case codes.DataLoss:
+		return &Error{http.StatusInternalServerError, "data_loss", desc}
+	default:
+		return err
+	}
 }
